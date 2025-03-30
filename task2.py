f42eeaebbdc3e27941dd656b9b3341d3b1f80d8f
AlexanderPirogovID <andrey.v.pirogov@gmail.com>
2025-03-30 15:32:15 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,12 @@
+try:
+    num1 = float(input("введите делимое: "))
+    num2 = float(input("введите делитеель: "))
+    res = num1 / num2
+except ZeroDivisionError:
+    print("ошибка: деление на ноль")
+except ValueError:
+    print("ошибка: это не число")
+except Exception as e:
+    print(f"неизвестная ошибка: {e}")
+else:
+    print(f"Результат деления {num1} на {num2} равен {res}")
